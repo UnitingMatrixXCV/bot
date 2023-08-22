b71012d7bb82a4b75408aeb90ba0d8192cd910ca
IThundxr <contact@ithundxr.dev>
2023-08-22 15:26:58 -0400

    remove update command
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/commands/_commands.ts
+++ src/commands/_commands.ts
@@ -4,7 +4,6 @@ import { listWarningsCommand } from './moderation/listwarnings.command';
 import { warnCommand } from './moderation/warn.command';
 import { sayCommand } from './util/say.command';
 import { tagCommand } from './util/tag.command';
-import { updateCommand } from './util/update.command';
 
 export const commands: Command[] = [
     sayCommand,
@@ -12,7 +11,6 @@ export const commands: Command[] = [
     warnCommand,
     listWarningsCommand,
     deleteWarningCommand,
-    updateCommand,
 ];
 
 export default commands;
