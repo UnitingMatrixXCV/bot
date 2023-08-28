715ba2a64ac9fb18cfb32de0fe77e3aa206429c7
IThundxr <contact@ithundxr.dev>
2023-08-28 10:01:00 -0400

    implode
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/logIssueAnalyzers/createVersion.ts
+++ src/logIssueAnalyzers/createVersion.ts
@@ -4,13 +4,12 @@ export const createVersionAnalyzer: Analyzer = async (log) => {
     const matchesCreate = log.mods
         ? log.mods.get('create') == '0.5.1.b'
         : log.content.match(/create-(.)+-0\.5\.1\.b/);
-    const matchesSNR = log.mods
-        ? log.mods.get('railways') == '1.5.0'
-        : log.content.match(/Steam_Rails-(.)+-1\.5\.0/);
+    if (!log.mods) return null;
+    const matchesSNR = log.mods.get('railways')?.includes('1.5.0');
     if (matchesCreate && matchesSNR) {
         return {
             name: 'Incompatible with Create 0.5.1b',
-            value: "Create: Steam 'n' Rails is currently incompatible with `Create 0.5.1b`. Upgrade to `Create 0.5.1c` or downgrade Steam 'n' Rails.",
+            value: "Create: Steam 'n' Rails `1.5.0` is incompatible with `Create 0.5.1b`. Upgrade to `Create 0.5.1c` or downgrade Steam 'n' Rails.",
         };
     }
     return null;
