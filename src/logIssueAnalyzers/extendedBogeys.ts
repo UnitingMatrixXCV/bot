dc47c4dd2a8fa928066a3b6e0680079bb4ea85ad
IThundxr <contact@ithundxr.dev>
2023-08-28 10:22:25 -0400

    this should let the bot check for EB version issue
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/logIssueAnalyzers/extendedBogeys.ts
+++ src/logIssueAnalyzers/extendedBogeys.ts
@@ -8,11 +8,7 @@ export const extendedBogeysAnalyzer: Analyzer = async (log) => {
         : log.content.match(
               /NoSuchMethodError: 'com\.jozufozu\.flywheel\.util\.transform\.Transform\[\]/
           );
-    const matchesCreate = log.mods
-        ? log.mods.get('create') == '0.5.1.b'
-        : log.content.match(/create-(.)+-0\.5\.1\.b/);
-    const matchesSNR = log.mods.get('railways')?.includes('1.5.0');
-    if (matchesExtendedBogeys && matchesCreate && matchesSNR) {
+    if (matchesExtendedBogeys) {
         return {
             name: 'Extended Bogeys',
             value: "Extended Bogeys has not yet updated to work with `0.5.1c/d`. Please downgrade to `0.5.1b`. And downgrade Steam 'n' Rails.",
