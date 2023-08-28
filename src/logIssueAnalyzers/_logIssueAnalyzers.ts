b6c883e48727fab93d332168583fd5e3b3439296
IThundxr <contact@ithundxr.dev>
2023-08-28 09:28:40 -0400

    Revert "chore: 0.5.1c compat works now"
    This reverts commit e54bc9eb648ef7d3dec0e2afe6d804cd363dc4b0.

--- src/logIssueAnalyzers/_logIssueAnalyzers.ts
+++ src/logIssueAnalyzers/_logIssueAnalyzers.ts
@@ -1,10 +1,12 @@
 import { Analyzer } from '../handlers/log.handler';
+import { createVersionAnalyzer } from './createVersion';
 import { minecraftVersionAnalyzer } from './minecraftVersion';
 import { optifineAnalyzer } from './optifine';
 
 export const logAnalyzers: Analyzer[] = [
     optifineAnalyzer,
     minecraftVersionAnalyzer,
+    createVersionAnalyzer,
 ];
 
 export default logAnalyzers;
