15d314806dd487df7ca63b84a434a643af24f242
IThundxr <contact@ithundxr.dev>
2023-08-28 09:53:48 -0400

    this should fix that
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/logIssueAnalyzers/createVersion.ts
+++ src/logIssueAnalyzers/createVersion.ts
@@ -5,7 +5,7 @@ export const createVersionAnalyzer: Analyzer = async (log) => {
         ? log.mods.get('create') == '0.5.1.b'
         : log.content.match(/create-(.)+-0\.5\.1\.b/);
     const matchesSNR = log.mods
-        ? log.mods.get('Steam_Rails') == '1.5.0'
+        ? log.mods.get('railways') == '1.5.0'
         : log.content.match(/Steam_Rails-(.)+-1\.5\.0/);
     if (matchesCreate && matchesSNR) {
         return {
