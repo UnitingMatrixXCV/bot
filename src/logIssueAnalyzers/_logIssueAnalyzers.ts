e54bc9eb648ef7d3dec0e2afe6d804cd363dc4b0
IThundxr <contact@ithundxr.dev>
2023-08-28 09:07:42 -0400

    chore: 0.5.1c compat works now
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/logIssueAnalyzers/_logIssueAnalyzers.ts
+++ src/logIssueAnalyzers/_logIssueAnalyzers.ts
@@ -1,12 +1,10 @@
 import { Analyzer } from '../handlers/log.handler';
-import { createVersionAnalyzer } from './createVersion';
 import { minecraftVersionAnalyzer } from './minecraftVersion';
 import { optifineAnalyzer } from './optifine';
 
 export const logAnalyzers: Analyzer[] = [
     optifineAnalyzer,
     minecraftVersionAnalyzer,
-    createVersionAnalyzer,
 ];
 
 export default logAnalyzers;
